568c64cd313a9bcb298f40ef0b1d34bee8abb46d
Catislin <caitlin.estrellita@gmail.com>
2017-12-10 14:47:11 -0800

    upgraded to material design

--- components/EventCard.js
+++ components/EventCard.js
@@ -14,6 +14,19 @@ const EventCard = ({name, date, address, id, placeId}) => (
 const styles = StyleSheet.create({
   eventContainer: {
     margin: 13,
+    padding: 14,
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
   },
   eventHeader: {
     display: 'flex',
