061b122711284aa57e570b3c1644f3e4099f3f22
Catislin <caitlin.estrellita@gmail.com>
2017-12-11 23:25:31 -0800

    fixed date and time pickers with icons

--- components/CreateEventForm.js
+++ components/CreateEventForm.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button, Image, TouchableOpacity, TouchableHighl
 import { FormLabel, FormInput, SearchBar } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 import { GooglePlacesAutocomplete } from 'react-native-google-places';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const PLACES = 'AIzaSyCo9YcZlx8POaoqjHVG2aTKThuoyCRjsVc';
 
@@ -25,32 +26,30 @@ class CreateEventForm extends Component {
     return(
       <View style={styles.container}>
         <FormLabel> Event Name </FormLabel>
-        <FormInput onChangeText={this.updateEventName}/>
+        <FormInput style={styles.input} onChangeText={this.updateEventName}/>
         <DatePicker
-          style={styles.dateStyle}
+          style={{width: 200}}
           date={this.state.date}
           mode="date"
-          placeholder="select date"
+          placeholder="Select date"
           format="YYYY-MM-DD"
           minDate="2016-05-01"
           maxDate="2016-06-01"
           confirmBtnText="Confirm"
+          iconComponent={<Icon name='event' size={30} />}
           cancelBtnText="Cancel"
-          customStyles={{
-            dateIcon: {
-              position: 'absolute',
-              left: 0,
-              top: 4,
-              marginLeft: 30,
-              marginTop: 15
-            },
-            dateInput: {
-              marginLeft: 66,
-              marginTop: 25
-            }
-
-          }}
-          onDateChange={(date) => {this.setState({date: date})}}
+          onDateChange={(date) => {this.setState({date: date});}}
+        />
+        <DatePicker
+          style={{width: 200}}
+          date={this.state.time}
+          mode="time"
+          format="HH:mm"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          minuteInterval={10}
+          iconComponent={<Icon name='access-time' size={30} />}
+          onDateChange={(time) => {this.setState({time: time});}}
         />
         <Button title="Submit" onPress={() => {console.log('submitting')}}/>
       </View>
@@ -60,14 +59,19 @@ class CreateEventForm extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    flexDirection: 'column',
-    height: 600
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   },
-  dateStyle: {
-    width: 300
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  input: {
+    padding: 5
   }
-})
+});
 
 
 export default CreateEventForm;
