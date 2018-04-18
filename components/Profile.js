07a37f137096b30c32e179854d5d408ed364316a
AlirieGray <caitlin.estrellita@gmail.com>
2018-04-18 11:23:52 -0700

    fixed distance and time in profile view

--- components/Profile.js
+++ components/Profile.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { StyleSheet, Text, View, ScrollView, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button, Dimensions, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import * as eventActions from '../actions/events';
 import * as modalActions from '../actions/modal';
 import EventCard from './EventCard';
 import CreateEventForm from './CreateEventForm';
+import moment from 'moment';
+var now = moment().format();
+
+// details for Google Maps View
+let { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LAT_DELTA = 0.008;
+const LNG_DELTA = LAT_DELTA / ASPECT_RATIO;
+
+const mapStyle = {
+    height: 400,
+    width: '100%'
+}
 
 
 class Profile extends Component {
@@ -16,12 +29,28 @@ class Profile extends Component {
     this.state = {
       showHosting: true
     }
+    this.getDistanceToEvent = this.getDistanceToEvent.bind(this);
+    this.deg2rad = this.deg2rad.bind(this);
+    this.getTimeUntilEvent = this.getTimeUntilEvent.bind(this);
   }
 
   componentDidMount() {
     console.log(this.props)
     this.props.getUserEvents(this.props.auth.userId);
     this.props.navigation.setParams({ setCreateEventModal: this.props.setCreateEventModal });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log("USER POSITION", position)
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          gotLocation: true
+        })
+        this.props.setLocation(position.coords)
+      },
+      (error) => console.log( error.message ),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
   }
 
   static navigationOptions = ({ navigation }) => ({
@@ -60,17 +89,45 @@ class Profile extends Component {
     </TouchableOpacity>
   });
 
+  getDistanceToEvent(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1);
+    var a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    d = d * 0.621371;
+    return Math.round(d * 10) / 10;
+  }
+
+  deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
+
+  getTimeUntilEvent(eventTime) {
+    var fromNow = moment(eventTime).fromNow();
+    return fromNow;
+  }
+
   render() {
     const created = this.props.userEvents.created;
     const attending = this.props.userEvents.attending;
     var haps = null;
     if (this.state.showHosting) {
       {haps = created.map((event, index) => {
-        return <EventCard key={event._id} {...event} {...this.props}  />
+        var distance = this.getDistanceToEvent(event.lat, event.lng, this.state.latitude, this.state.longitude);
+        var timeUntil = this.getTimeUntilEvent(event.date)
+        return <EventCard key={event._id} {...event} distance={distance} timeUntil={timeUntil} {...this.props}  />
       })}
     } else {
       {haps = attending.map((event, index) => {
-        return <EventCard key={event._id} {...event} {...this.props}  />
+        var distance = this.getDistanceToEvent(event.lat, event.lng, this.state.latitude, this.state.longitude);
+        var timeUntil = this.getTimeUntilEvent(event.date)
+        return <EventCard key={event._id} {...event} distance={distance} timeUntil={timeUntil} {...this.props}  />
       })}
     }
     return(
